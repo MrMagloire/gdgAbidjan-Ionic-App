093a9d955fd50c5c6decff1f153c749f19579414
Mr Magloire <angyaom@gmail.com>
2017-06-18 20:50:11 +0000

    Verification de connexion dans l'event

--- src/pages/event/event.ts
+++ src/pages/event/event.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
 
 import { InAppBrowser } from '@ionic-native/in-app-browser';
+
+// Network connexion
+import { Network } from '@ionic-native/network';
+import { ConnectivityService } from '../../providers/connectivity-service';
+declare var meetup;
 
 @Component({
   selector: 'page-event',
@@ -12,19 +17,50 @@ import { InAppBrowser } from '@ionic-native/in-app-browser';
 export class EventPage {
 
   MeetupEvents: any;
-  constructor(public navCtrl: NavController, public http: Http, private iab: InAppBrowser) {
+  constructor(public navCtrl: NavController, public http: Http, private iab: InAppBrowser, private alertCtrl: AlertController, public network: Network, public connectivityService: ConnectivityService) {
     this.MeetupEvents = [];
-    this.http.get('https://api.meetup.com/2/events?&group_urlname=GDGAbidjan&page=20').map(res => res.json()).subscribe(data => {
-        this.MeetupEvents = data.results;
-        console.log(this.MeetupEvents);
-    }
-    ,
-    err => {
-        console.log("Oops!");
-    }
-    );
+    this.loadEvent();
   }
   launch(url) {
     this.iab.create(url,'_blank');
   }
+
+  loadEvent() {
+    this.addConnectivityListeners();
+    if(this.connectivityService.isOnline()){
+        console.log("online");
+        this.http.get('https://api.meetup.com/2/events?&group_urlname=GDGAbidjan&page=20').map(res => res.json()).subscribe(data => {
+            this.MeetupEvents = data.results;
+            console.log(this.MeetupEvents);
+        },
+        err => {
+            console.log("Oops!");
+        });
+    }
+    else{
+      this.presentAlert();
+    }
+  }
+
+  addConnectivityListeners(){
+    let onOnline = () => {
+      setTimeout(() => {
+        if(typeof meetup == "undefined"){
+          this.loadEvent();
+        }
+      }, 5000);
+    };
+    document.addEventListener('online', onOnline, false);
+
+  }
+
+  presentAlert() {
+    let alert = this.alertCtrl.create({
+      title: 'Connexion',
+      subTitle: 'Verifier votre connexion internet',
+      buttons: ['Ok']
+    });
+    alert.present();
+  }
+
 }
